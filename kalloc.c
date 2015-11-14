a123d0750e82104f7c225aa6eb3195107dafd9bc
forwchen <forwchen@gmail.com>
2015-11-14 21:16:48 +0800

    replace memory allocator

--- kalloc.c
+++ kalloc.c
@@ -8,19 +8,30 @@
 #include "memlayout.h"
 #include "mmu.h"
 #include "spinlock.h"
+#include "queue.h"
 
 void freerange(void *vstart, void *vend);
 extern char end[]; // first address after kernel loaded from ELF file
 
+typedef LIST_HEAD(run_list, run) list_head;
+typedef LIST_ENTRY(run) list_entry;
+
 struct run {
-  struct run *next;
+  uint flags;
+  uint size; // size in page
+  //struct run *next;
+  list_entry next;
 };
 
 struct {
   struct spinlock lock;
   int use_lock;
-  struct run *freelist;
+  uint nfreeblock;
+  //struct run *freelist;
+  list_head freelist;
 } kmem;
+
+
 
 // Initialization happens in two phases.
 // 1. main() calls kinit1() while still using entrypgdir to place just
@@ -31,7 +42,9 @@ void
 kinit1(void *vstart, void *vend)
 {
   initlock(&kmem.lock, "kmem");
+  LIST_INIT(&kmem.freelist);
   kmem.use_lock = 0;
+  kmem.nfreeblock = 0;
   freerange(vstart, vend);
 }
 
@@ -45,10 +58,27 @@ kinit2(void *vstart, void *vend)
 void
 freerange(void *vstart, void *vend)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint)vstart);
-  for(; p + PGSIZE <= (char*)vend; p += PGSIZE)
-    kfree(p);
+//  char *p;
+//  p = (char*)PGROUNDUP((uint)vstart);
+//  for(; p + PGSIZE <= (char*)vend; p += PGSIZE)
+//    kfree(p);
+  char *s = (char*)PGROUNDUP((uint)vstart);
+  char *e = (char*)PGROUNDUP((uint)vend);
+  struct run *r = (struct run*)s;
+  r->size = ((uint)e-(uint)s) / PGSIZE;
+  LIST_INSERT_HEAD(&kmem.freelist, r, next);
+  kmem.nfreeblock++;
+}
+
+void print_mem()
+{
+  struct run *r = LIST_FIRST(&kmem.freelist);
+  int count = 0;
+  while (count < kmem.nfreeblock) {
+    cprintf("%x\t\t\t%d\n", r, r->size);
+    count ++;
+    r = LIST_NEXT(r, next);
+  }
 }
 
 //PAGEBREAK: 21
@@ -59,19 +89,51 @@ freerange(void *vstart, void *vend)
 void
 kfree(char *v)
 {
-  struct run *r;
-
+  //struct run *r;
+  //cprintf("kfree %x\n", v);
+  //print_mem();
   if((uint)v % PGSIZE || v < end || v2p(v) >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
-  memset(v, 1, PGSIZE);
+  // memset(v, 1, PGSIZE);
 
   if(kmem.use_lock)
     acquire(&kmem.lock);
-  r = (struct run*)v;
-  r->next = kmem.freelist;
-  kmem.freelist = r;
+  //r = (struct run*)v;
+  //r->next = kmem.freelist;
+  //kmem.freelist = r;
+  int n = 1;
+  struct run *p = (struct run*)v; // page(s) to free
+  struct run *r = LIST_FIRST(&kmem.freelist);
+  p->size = n;
+  int count = 0;
+  int merged = 0;
+  while (count < kmem.nfreeblock)
+  {
+    if ((char *)r == v + p->size * PGSIZE)
+    {
+      p->size += r->size;
+      LIST_INSERT_BEFORE(r, p, next);
+      LIST_REMOVE(r, next);
+      merged = 1;
+      break;
+    }
+    else if ((char *)r + r->size * PGSIZE == v)
+    {
+      r->size += p->size;
+      merged = 1;
+      break;
+    }
+    count ++;
+    r = LIST_NEXT(r, next);
+  }
+  if (merged == 0)
+  {
+    LIST_INSERT_HEAD(&kmem.freelist, p, next);
+    kmem.nfreeblock ++;
+  }
+
   if(kmem.use_lock)
     release(&kmem.lock);
 }
@@ -82,15 +144,47 @@ kfree(char *v)
 char*
 kalloc(void)
 {
-  struct run *r;
-
+  //struct run *r;
+  //print_mem();
   if(kmem.use_lock)
     acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
+  int n = 1; // n pages to alloc
+  struct run *r = LIST_FIRST(&kmem.freelist);
+  int count = 0;
+  while (count < kmem.nfreeblock) {
+    if (r->size >= n) break;
+    count ++;
+    r = LIST_NEXT(r, next);
+  }
+  if (count < kmem.nfreeblock){
+    LIST_REMOVE(r, next);
+    struct run* remain = (struct run*)((char *)r + n * PGSIZE);
+    remain->size = r->size - n;
+    r->size = n;
+    if (remain->size > 0)
+      LIST_INSERT_HEAD(&kmem.freelist, remain, next);
+    else
+      kmem.nfreeblock--;
+    if(kmem.use_lock)
+    release(&kmem.lock);
+    //cprintf("kalloc %x\n", r);
+    return (char*)r;
+  }
+  else {
+    //cprintf("cannot allocate\n");
+    if(kmem.use_lock)
+    release(&kmem.lock); 
+    return NULL;
+  }
+
+//  r = kmem.freelist;
+//  if(r)
+//    kmem.freelist = r->next;
+
+  // will not execute anyway
   if(kmem.use_lock)
     release(&kmem.lock);
   return (char*)r;
 }
+
 
